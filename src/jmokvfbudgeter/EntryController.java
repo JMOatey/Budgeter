1a62238501b2df0da4a875be8843cc8e76741128
JohnMichael <jmokvf@mail.missouri.edu>
2018-12-01 11:52:58 -0600

    Add info

--- src/jmokvfbudgeter/EntryController.java
+++ src/jmokvfbudgeter/EntryController.java
@@ -10,6 +10,7 @@ import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.TextArea;
 
 /**
  *
@@ -17,10 +18,20 @@ import javafx.fxml.Initializable;
  */
 public class EntryController extends Switchable implements Initializable {
     
+    @FXML
+    TextArea info;
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        info.setText(
+                "Thank you for trying my app. It's primary use case is for families to manage all of their "
+                        + "accounts (Credit/Debit). This can let parents know what's going on with all of their children "
+                        + "as well as their own. Click the Budget button to start now! I hope it helps make life easier\n\n"
+                        + "Now a little bit about me. My name is John Michael. I am a Junior in Computer Science. "
+                        + "I have a huge passion for cs and want to use it to create the best applications I can "
+                        + "in my career. I want to try working in the vido game industry to possibly make a game "
+                        + "that can influence people in the same way they have me."
+        );
     }  
     
     @FXML
